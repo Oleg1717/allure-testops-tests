165237e699683e1108f2b4a06ea646a0613e0eaa
Oleg1717 <olekmail@yandex.ru>
2021-10-22 17:05:48 +0300

    added dashboard widgets api

---
+++ src/test/java/cloud/autotests/api/requests/DashboardWidgetsRequests.java
@@ -0,0 +1,57 @@
+package cloud.autotests.api.requests;
+
+import cloud.autotests.api.helpers.EndPoints;
+import cloud.autotests.api.models.dashboards.Dashboard;
+import cloud.autotests.api.models.dashboards.Widget;
+import cloud.autotests.api.models.dashboards.WidgetData;
+import io.restassured.http.ContentType;
+
+import static cloud.autotests.api.helpers.RestAssuredSpec.spec;
+import static io.restassured.RestAssured.given;
+
+public class DashboardWidgetsRequests {
+
+    public static WidgetData getWidgetDataRequest(int widgetId) {
+        return given()
+                .spec(spec().request())
+                .when()
+                .get(EndPoints.WIDGET_DATA, widgetId)
+                .then()
+                .statusCode(200)
+                .extract().as(WidgetData.class);
+    }
+
+    public static Widget addWidgetRequest(Widget widget) {
+        return given()
+                .spec(spec().request())
+                .contentType(ContentType.JSON)
+                .body(widget)
+                .when()
+                .post(EndPoints.WIDGET)
+                .then()
+                .statusCode(200)
+                .extract().as(Widget.class);
+    }
+
+    public static Widget editWidgetRequest(Widget widget) {
+        return given()
+                .spec(spec().request())
+                .contentType(ContentType.JSON)
+                .body(widget)
+                .when()
+                .patch(EndPoints.WIDGET_ID, widget.getId())
+                .then()
+                .statusCode(200)
+                .extract().as(Widget.class);
+    }
+
+    public void deleteWidget(int widgetId) {
+        given()
+                .spec(spec().request())
+                .pathParam("id", widgetId)
+                .when()
+                .delete(EndPoints.WIDGET_ID)
+                .then()
+                .statusCode(202);
+    }
+}
--- src/test/java/cloud/autotests/api/requests/DashboardsRequests.java
+++ src/test/java/cloud/autotests/api/requests/DashboardsRequests.java
@@ -1,6 +1,71 @@
 package cloud.autotests.api.requests;
 
+import cloud.autotests.api.helpers.EndPoints;
+import cloud.autotests.api.models.dashboards.Dashboard;
+import cloud.autotests.api.models.dashboards.Dashboards;
+import cloud.autotests.api.models.dashboards.Widget;
+import cloud.autotests.api.models.projects.Project;
+import io.restassured.http.ContentType;
+
+import java.util.List;
+
+import static cloud.autotests.api.helpers.RestAssuredSpec.spec;
+import static io.restassured.RestAssured.given;
+
 public class DashboardsRequests {
 
+    public static Dashboards getDashboardsDataRequest(int projectId) {
+        return given()
+                .spec(spec().request())
+                .param("projectId", projectId)
+                .param("size", "500")
+                .when()
+                .get(EndPoints.DASHBOARD)
+                .then()
+                .statusCode(200)
+                .extract().as(Dashboards.class);
+    }
 
+    public static Dashboard getDashboardDataRequest(int dashboardId) {
+        return given()
+                .spec(spec().request())
+                .when()
+                .get(EndPoints.DASHBOARD_ID, dashboardId)
+                .then()
+                .statusCode(200)
+                .extract().as(Dashboard.class);
+    }
+
+    public static Dashboard addDashboardRequest(Dashboard dashboard) {
+        return given()
+                .spec(spec().request())
+                .contentType(ContentType.JSON)
+                .body(dashboard)
+                .when()
+                .post(EndPoints.DASHBOARD)
+                .then()
+                .statusCode(200)
+                .extract().as(Dashboard.class);
+    }
+
+    public static Dashboard editDashboardRequest(Dashboard dashboardData) {
+        return given()
+                .spec(spec().request())
+                .contentType(ContentType.JSON)
+                .body(dashboardData)
+                .when()
+                .patch(EndPoints.DASHBOARD_ID, dashboardData.getId())
+                .then()
+                .statusCode(200)
+                .extract().as(Dashboard.class);
+    }
+
+    public static void deleteDashboardRequest(int dashboardId) {
+        given()
+                .spec(spec().request())
+                .when()
+                .delete(EndPoints.DASHBOARD_ID, dashboardId)
+                .then()
+                .statusCode(202);
+    }
 }
