164dc7c1cb50e20d1e751e1340ab196a26bbe3bf
Oleg1717 <olekmail@yandex.ru>
2021-10-24 22:13:28 +0300

    api changed

--- src/test/java/cloud/autotests/allure/api/models/dashboards/WidgetData.java
+++ src/test/java/cloud/autotests/allure/api/models/dashboards/WidgetData.java
@@ -2,7 +2,6 @@ package cloud.autotests.allure.api.models.dashboards;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +9,6 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor
-@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class WidgetData {
