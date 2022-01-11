8e290626b64fd7e9eda72c431e6e76a272cd2d54
Oleg1717 <olekmail@yandex.ru>
2022-01-12 00:47:48 +0300

    change AuthData

--- src/test/java/cloud/autotests/allure/api/helpers/AuthData.java
+++ src/test/java/cloud/autotests/allure/api/helpers/AuthData.java
@@ -3,9 +3,6 @@ package cloud.autotests.allure.api.helpers;
 import cloud.autotests.allure.api.steps.UserApi;
 import cloud.autotests.allure.config.ConfigHelper;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class AuthData {
 
     private final String xsrfToken = String.valueOf((int) (Math.random() * 1000));
@@ -14,7 +11,6 @@ public class AuthData {
     private AuthData() {
         sessionToken = new UserApi()
                 .getSessionToken(xsrfToken, ConfigHelper.getMainUserLogin(), ConfigHelper.getMainUserPassword());
-        System.out.println(sessionToken);
     }
 
     public static AuthData getAuthData() {
@@ -25,7 +21,9 @@ public class AuthData {
         private static final AuthData AUTH_DATA = new AuthData();
     }
 
-    public String xsrfToken() { return xsrfToken; }
+    public String xsrfToken() {
+        return xsrfToken;
+    }
 
     public String sessionToken() {
         return sessionToken;
