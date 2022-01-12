908a603b1471a8f18285378af1d9f8c2fdfba017
Oleg1717 <olekmail@yandex.ru>
2022-01-12 15:22:16 +0300

    change JobData

--- src/test/java/cloud/autotests/allure/api/testdata/JobData.java
+++ src/test/java/cloud/autotests/allure/api/testdata/JobData.java
@@ -3,35 +3,41 @@ package cloud.autotests.allure.api.testdata;
 import cloud.autotests.allure.api.models.jobs.Job;
 import cloud.autotests.allure.api.models.jobs.JobParameter;
 import cloud.autotests.allure.config.ConfigHelper;
+import com.github.javafaker.Faker;
 
 import java.util.Arrays;
 
 public class JobData {
 
-    private static JobParameter allureNotificationVersion = JobParameter.builder()
-            .name("ALLURE_NOTIFICATIONS_VERSION")
-            .defaultValue("3.1.1")
-            .build();
+    private Faker faker = new Faker();
 
-    private static JobParameter threads = JobParameter.builder()
-            .name("THREADS")
-            .defaultValue("5")
-            .build();
+    public Job getMinJobData() {
+        return Job.builder()
+                .projectId(ConfigHelper.getProjectId())
+                .name("co7-ov-regform-min-" + faker.idNumber())
+                .type("jenkins")
+                .build();
+    }
 
-    public static Job minJobData = Job.builder()
-            .projectId(ConfigHelper.getProjectId())
-            .name("co7-ov-regform-min")
-            .type("jenkins")
-            .build();
+    public Job getMaxJobData() {
+        JobParameter allureNotificationsVersion = getJobParameter("ALLURE_NOTIFICATIONS_VERSION", "3.1.1");
+        JobParameter threads = getJobParameter("THREADS", "5");
+        return Job.builder()
+                .projectId(ConfigHelper.getProjectId())
+                .canRun(true)
+                .buildServerId(1)
+                .name("co7-ov-regform-max-" + faker.idNumber())
+                .url("https://jenkins.autotests.cloud/job/c07-ov-regform/")
+                .type("jenkins")
+                .externalId("co7-ov-regform-max")
+                .parameters(Arrays.asList(allureNotificationsVersion, threads))
+                .build();
+    }
 
-    public static Job maxJobData = Job.builder()
-            .projectId(ConfigHelper.getProjectId())
-            .canRun(true)
-            .buildServerId(1)
-            .name("co7-ov-regform-max")
-            .url("https://jenkins.autotests.cloud/job/c07-ov-regform/")
-            .type("jenkins")
-            .externalId("co7-ov-regform-max")
-            .parameters(Arrays.asList(allureNotificationVersion, threads))
-            .build();
+    private JobParameter getJobParameter(String name, String value) {
+        return JobParameter.builder()
+                .name(name)
+                .defaultValue(value)
+                .build();
+    }
 }
