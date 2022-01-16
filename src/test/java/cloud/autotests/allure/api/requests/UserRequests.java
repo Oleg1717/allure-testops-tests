f77fc816032f18c6d358708f687679ec81aef741
Oleg1717 <olekmail@yandex.ru>
2022-01-17 00:08:38 +0300

    change CustomLogFilter

--- src/test/java/cloud/autotests/allure/api/requests/UserRequests.java
+++ src/test/java/cloud/autotests/allure/api/requests/UserRequests.java
@@ -1,10 +1,11 @@
 package cloud.autotests.allure.api.requests;
 
 import cloud.autotests.allure.api.data.ApiEndpoint;
-import cloud.autotests.allure.api.helpers.RestAssuredFilter;
+import cloud.autotests.allure.api.helpers.CustomLogFilter;
 import cloud.autotests.allure.config.ConfigHelper;
 import io.restassured.response.Response;
 
+import static cloud.autotests.allure.api.helpers.CustomLogFilter.customLogFilter;
 import static cloud.autotests.allure.api.helpers.RestAssuredSpec.spec;
 import static io.restassured.RestAssured.given;
 
@@ -13,7 +14,7 @@ public class UserRequests {
     public static Response getAuthorizeResponse(String xsrfToken, String username, String password) {
         return given()
                 .baseUri(ConfigHelper.getBaseUrl())
-                .filter(RestAssuredFilter.withCustomTemplates())
+                .filter(customLogFilter().withCustomTemplates())
                 .header("X-XSRF-TOKEN", xsrfToken)
                 .cookie("XSRF-TOKEN", xsrfToken)
                 .formParam("username", username)
